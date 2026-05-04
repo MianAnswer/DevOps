335ef8e0e4910f441e98d3df2c5f1379957680f6
Mian Answer <mianwanswer@gmail.com>
2026-05-04 11:39:08 -0500

    Restructure code for modularization

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
     res.send('Hello, World!\n');
 });
 
-app.all('/', (req, res) => {
+app.get('/health', (req, res) => {
+    res.status(200).send('OK\n');
+});
+
+app.use('/', (req, res) => {
     res.status(404).send('404 Not Found\n');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+export default app;
